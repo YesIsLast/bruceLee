3f721fc8e4fb8a39c354822ca1c3a52a41bf597a
weboth <flykeying@126.com>
2019-12-13 16:13:40 +0800

    修改消息列表样式

--- unpackage/dist/dev/app-plus/pages/wechat/index.js
+++ unpackage/dist/dev/app-plus/pages/wechat/index.js
@@ -1,1 +1,2 @@
+setCssToHead([],undefined,{path:"./pages/wechat/index.wxss"})();
 document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/wechat/index.wxml') } }));
